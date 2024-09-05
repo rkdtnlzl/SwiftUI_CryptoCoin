8f19c1ec7419fd2203975701c6f3752495b40717
rkdtnlzl <rkdtnlzl@naver.com>
2024-09-05 21:16:03 +0900

    Initial Commit

---
+++ SwiftUI_CryptoCoin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_CryptoCoin
+//
+//  Created by 강석호 on 9/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_CryptoCoin/SwiftUI_CryptoCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CryptoCoinApp.swift
+//  SwiftUI_CryptoCoin
+//
+//  Created by 강석호 on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CryptoCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
