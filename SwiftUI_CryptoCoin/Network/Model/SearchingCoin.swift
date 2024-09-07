7fe5501fd03820081c44b03277c07f9dbd5538a9
rkdtnlzl <rkdtnlzl@naver.com>
2024-09-07 17:48:16 +0900

    [Add] SearchingCoin 모델 추가

---
+++ SwiftUI_CryptoCoin/Network/Model/SearchingCoin.swift
@@ -0,0 +1,20 @@
+//
+//  SearchingCoin.swift
+//  SwiftUI_CryptoCoin
+//
+//  Created by 강석호 on 9/7/24.
+//
+
+import Foundation
+
+struct SearchingCoin: Hashable, Identifiable, Decodable {
+    let id: String
+    let name: String
+    let symbol: String
+    let thumb: String
+    
+}
+
+struct SearchingCoinsResponse: Decodable {
+    let coins: [SearchingCoin]
+}
